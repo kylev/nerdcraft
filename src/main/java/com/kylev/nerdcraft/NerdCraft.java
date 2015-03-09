8d37f2c37c3cef9797b10859ee49981d4bcb8cf0
Kyle VanderBeek <kylev@kylev.com>
2015-03-09 01:40:09 -0700

    Basic mod base

---
+++ src/main/java/com/kylev/nerdcraft/NerdCraft.java
@@ -0,0 +1,27 @@
+package com.kylev.nerdcraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "NerdCraft", name = "Nerd Craft", version = "1.7.2-0.1.0")
+public class NerdCraft {
+    @Mod.Instance("NerdCraft")
+    public static NerdCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
