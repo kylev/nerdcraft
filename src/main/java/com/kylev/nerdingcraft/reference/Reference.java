f34774654208ceeffea8102b6f00b7605ab0c6b8
Kyle VanderBeek <kylev@kylev.com>
2015-03-11 18:10:36 -0700

    Corrected names and versions.

--- src/main/java/com/kylev/nerdingcraft/reference/Reference.java
+++ src/main/java/com/kylev/nerdingcraft/reference/Reference.java
@@ -1,9 +1,9 @@
 package com.kylev.nerdingcraft.reference;
 
 public class Reference {
-    public static final String MOD_ID = "NerdCraft";
-    public static final String MOD_NAME = "NerdCraft";
-    public static final String VERSION = "1.7.2-0.1.0";
+    public static final String MOD_ID = "NerdingCraft";
+    public static final String MOD_NAME = "Nerding Craft";
+    public static final String VERSION = "1.7.10-0.1.0";
     public static final String CLIENT_PROXY_CLASS = "com.kylev.nerdingcraft.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.kylev.nerdingcraft.proxy.ServerProxy";
 }
