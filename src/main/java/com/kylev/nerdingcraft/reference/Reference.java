98e99393246373f4a1abac5ce5123ee39c2d9f3f
Kyle VanderBeek <kylev@kylev.com>
2015-03-12 16:31:58 -0700

    Move versions and such to gradle.properties and automatically set them in mcmod.info

--- src/main/java/com/kylev/nerdingcraft/reference/Reference.java
+++ src/main/java/com/kylev/nerdingcraft/reference/Reference.java
@@ -3,7 +3,7 @@ package com.kylev.nerdingcraft.reference;
 public class Reference {
     public static final String MOD_ID = "NerdingCraft";
     public static final String MOD_NAME = "Nerding Craft";
-    public static final String VERSION = "1.7.10-0.1.0";
+    public static final String VERSION = "0.1.2";
     public static final String CLIENT_PROXY_CLASS = "com.kylev.nerdingcraft.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.kylev.nerdingcraft.proxy.ServerProxy";
 }
