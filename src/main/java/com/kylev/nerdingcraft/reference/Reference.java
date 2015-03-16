200f8cdb3d4e4801c1bc66adc93549821ae092ba
Kyle VanderBeek <kylev@kylev.com>
2015-03-16 01:16:22 -0700

    Just add a few Javadocs

--- src/main/java/com/kylev/nerdingcraft/reference/Reference.java
+++ src/main/java/com/kylev/nerdingcraft/reference/Reference.java
@@ -1,7 +1,12 @@
 package com.kylev.nerdingcraft.reference;
 
+/**
+ * Stuff.
+ */
 public class Reference {
+    /** The mod's ID. */
     public static final String MOD_ID = "NerdingCraft";
+    /** The mod's human-readable name. */
     public static final String MOD_NAME = "Nerding Craft";
     public static final String VERSION = "@VERSION@";
     public static final String CLIENT_PROXY_CLASS = "com.kylev.nerdingcraft.proxy.ClientProxy";
