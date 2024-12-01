d628cda043ccb72b679259fa83b8b0c9567af7d7
Kyle VanderBeek <kylev@kylev.com>
2024-11-30 19:57:53 -0800

    Tidy a bit.

--- src/main/java/com/kylev/nerdingcraft/NerdCraft.java
+++ src/main/java/com/kylev/nerdingcraft/NerdCraft.java
@@ -1,20 +1,10 @@
 package com.kylev.nerdingcraft;
 
 import org.slf4j.Logger;
-
-import com.kylev.nerdingcraft.blocks.WoolFurnace;
-import com.kylev.nerdingcraft.configuration.ConfigurationHandler;
-import com.kylev.nerdingcraft.reference.Reference;
 import com.mojang.logging.LogUtils;
-
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.FurnaceBlock;
-import net.minecraft.client.gui.components.tabs.Tab;
-import net.minecraft.client.resources.model.Material;
-import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -24,18 +14,17 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import com.kylev.nerdingcraft.reference.Reference;
+import com.kylev.nerdingcraft.blocks.WoolFurnace;
 
-@Mod ( Reference.MOD_ID)
+@Mod(Reference.MOD_ID)
 public class NerdCraft {
     public static Logger LOGGER = LogUtils.getLogger();
 
     // Registries
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Reference.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
+            Reference.MOD_ID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Reference.MOD_ID);
-    // public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister
-    //         .create(ForgeRegistries.BLOCK_ENTITY_TYPES, Reference.MOD_ID);
-    // public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(ForgeRegistries.MENU_TYPES,
-    // Reference.MOD_ID);
 
     // Static registry objects
     public static final RegistryObject<Block> SMILING_BLOCK = BLOCKS.register("smiling_block",
@@ -54,5 +43,4 @@ public class NerdCraft {
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
     }
-
 }
