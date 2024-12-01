beacf354ad91d7a91fc6f58979afed88cb6c271f
Kyle VanderBeek <kylev@kylev.com>
2024-11-30 19:50:51 -0800

    Monster revamp WIP; smiling block working.

--- src/main/java/com/kylev/nerdingcraft/blocks/WoolFurnace.java
+++ src/main/java/com/kylev/nerdingcraft/blocks/WoolFurnace.java
@@ -1,45 +1,12 @@
 package com.kylev.nerdingcraft.blocks;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.world.level.block.FurnaceBlock;
 
 /**
- * Created by kylev on 5/20/18.
+ * A furnace made of wool.
  */
-public class WoolFurnace extends Block {
-    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
-
-    public WoolFurnace() {
-        super(Material.CLOTH);
-        this.setCreativeTab(CreativeTabs.MISC);
-        setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-    }
-
-    @Override
-    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, FACING);
-    }
-
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(FACING).getHorizontalIndex();
+public class WoolFurnace extends FurnaceBlock {
+    public WoolFurnace(Properties properties) {
+        super(properties);
     }
 }
--- src/main/java/com/kylev/nerdingcraft/proxy/IProxy.java
+++
@@ -1,9 +0,0 @@
-package com.kylev.nerdingcraft.proxy;
-
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-public interface IProxy {
-    void preInit(FMLPreInitializationEvent event);
-
-    void registerRenderers();
-}
